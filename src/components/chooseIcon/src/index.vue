4302459c5e243cb6982352fbb6bee64e1428445d
Hzeo227 <zxveren@gmail.com>
2023-03-10 20:22:06 +0800

    Update index.vue

--- src/components/chooseIcon/src/index.vue
+++ src/components/chooseIcon/src/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { watch, ref } from 'vue'
+import { watch } from 'vue'
 
 let props = defineProps<{
   // 弹出框的标题
@@ -15,31 +15,20 @@ let handleClick = () => {
   emits('update:visible', !props.visible)
 }
 
-// 拷贝一份父组件传过来的 visible
-// 因为子组件尽量不要直接修改父组件传过来的 props 属性
-let dialogVisible = ref<boolean>(props.visible)
-
 watch(
   () => props.visible,
   (val) => {
-    dialogVisible.value = val
     emits('update:visible', val)
   }
 )
 
-watch(
-  () => dialogVisible.value,
-  (val) => {
-    emits('update:visible', val)
-  }
-)
 </script>
 
 <template>
   <el-button @click="handleClick" type="primary">
     <slot></slot>
   </el-button>
-  <el-dialog :title="title" v-model="dialogVisible">111</el-dialog>
+  <el-dialog :title="title" v-model="props.visible">111</el-dialog>
 </template>
 
 <style lang="scss" scoped>
