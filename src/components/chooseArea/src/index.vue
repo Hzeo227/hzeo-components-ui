398cead5a450fb4a8a8b1ffa517bec21a7044ada
Hzeo227 <zxveren@gmail.com>
2023-03-11 20:56:24 +0800

    feat: 省市区三级联动

---
+++ src/components/chooseArea/src/index.vue
@@ -0,0 +1,118 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import allAreas from '../lib/pca-code.json'
+
+export interface AreaItem {
+  code: string
+  name: string
+  children?: AreaItem[]
+}
+
+export interface Data {
+  code: string
+  name: string
+}
+
+// 所有省市区的数据
+let areas = ref(allAreas)
+// 省份下拉框的值
+let province = ref<string>()
+// 城市下拉框的值
+let city = ref<string>()
+// 区域下拉框的值
+let area = ref<string>()
+
+// 分发事件给父组件
+let emits = defineEmits(['change'])
+
+// 城市下拉框的所有值
+let selectCity = ref<AreaItem[]>([])
+watch(
+  () => province.value,
+  (val) => {
+    if (val) {
+      if (!province.value) return []
+      selectCity.value = areas.value.find((item) => item.code === province.value)!.children
+    }
+    city.value = ''
+    area.value = ''
+  }
+)
+
+// 区域下拉框的所有值
+let selectArea = ref<AreaItem[]>([])
+watch(
+  () => city.value,
+  (val) => {
+    if (val) {
+      if (!city.value) return []
+      selectArea.value = selectCity.value.find((item) => item.code === city.value)!.children!
+      area.value = ''
+    }
+  }
+)
+
+// 监听选择区域值
+watch(
+  () => area.value,
+  (val) => {
+    if (val) {
+      let provinceData: Data = {
+        code: province.value!,
+        name: province.value! && allAreas.find((item) => item.code === province.value)!.name
+      }
+      let cityData: Data = {
+        code: city.value!,
+        name: city.value! && selectCity.value.find((item) => item.code === city.value)!.name
+      }
+      let areaData: Data = {
+        code: val,
+        name: area.value! && selectArea.value.find((item) => item.code === val)!.name
+      }
+
+      emits('change', {
+        province: provinceData,
+        city: cityData,
+        area: areaData
+      })
+    }
+  }
+)
+</script>
+
+<template>
+  <el-select placeholder="请选择省份" v-model="province" size="large" clearable>
+    <el-option v-for="item in areas" :key="item.code" :label="item.name" :value="item.code" />
+  </el-select>
+  <el-select
+    :disabled="!province"
+    placeholder="请选择城市"
+    style="margin: 0 15px"
+    v-model="city"
+    size="large"
+    clearable
+  >
+    <el-option
+      v-for="item in selectCity"
+      :key="item.code"
+      :label="item.name"
+      :value="item.code"
+    ></el-option>
+  </el-select>
+  <el-select
+    :disabled="!province || !city"
+    placeholder="请选择区域"
+    v-model="area"
+    size="large"
+    clearable
+  >
+    <el-option
+      v-for="item in selectArea"
+      :key="item.code"
+      :label="item.name"
+      :value="item.code"
+    ></el-option>
+  </el-select>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/views/chooseArea/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import ChooseArea, { type AreaItem } from '@/components/chooseArea/src/index.vue'
+
+export interface AreaInfo {
+  province: AreaItem
+  city: AreaItem
+  area: AreaItem
+}
+
+const changeArea = (res: AreaInfo) => {
+  console.log(res)
+}
+</script>
+
+<template>
+  <choose-area @change="changeArea"></choose-area>
+</template>
+
+<style lang="scss" scoped></style>
